7e59d4a288cd4c913772f7b26a5080c1ffa41d52
Mohd Faraz <mohd.faraz.abc@gmail.com>
2018-04-06 17:47:32 +0000

    mido: Fix Kernel Issue

--- omni_mido.mk
+++ omni_mido.mk
@@ -30,6 +30,10 @@ PRODUCT_PACKAGES += \
 	charger_res_images \
 	charger
 
+# Kernel
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/Image.gz-dtb:kernel
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
 PRODUCT_NAME := omni_mido
