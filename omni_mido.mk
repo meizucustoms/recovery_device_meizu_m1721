74c4b5d4a6e921707d8d7dbd97223388da86a673
rezaadi0105 <rezaadipangestu5@gmail.com>
2019-05-01 00:26:04 +0700

    update to pie

--- omni_mido.mk
+++ omni_mido.mk
@@ -14,28 +14,16 @@
 # limitations under the License.
 #
 
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/omni/config/gsm.mk)
+# Release name
+PRODUCT_RELEASE_NAME := mido
+
+$(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-
-# Inherit Telephony packages
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit language packages
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.keystore=msm8953
-
-# Charger
-PRODUCT_PACKAGES += \
-    charger_res_images
-
-# Encryption
-PRODUCT_PACKAGES += \
-    libcryptfs_hw
 
 # Kernel
 PRODUCT_COPY_FILES += \
