598aa39cde31e598089b342b0e4f87883ba4c1dd
Darth9 <reyprimerjefe@gmail.com>
2021-05-07 17:50:42 +0100

    Update the lunch combo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -83,7 +83,8 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
   	   export | grep "PLATFORM_" >> $FOX_BUILD_LOG_FILE
   	fi
 
-	add_lunch_combo omni_"$FDEVICE"-eng
-	add_lunch_combo omni_"$FDEVICE"-userdebug
+  	for var in eng user userdebug; do
+  		add_lunch_combo omni_"$FDEVICE"-$var
+  	done
 fi
 #
