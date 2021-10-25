03dd923b7bade139e15007210af77dcf9b8fecc3
Roman Rihter <teledurak@gmail.com>
2021-10-25 19:33:35 +0300

    m1721: fix config errors

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -62,7 +62,6 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI="1"
 
 	# OTA for custom ROMs
-	export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
 	export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
 
 	# -- add settings for R11 --
